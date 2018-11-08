9d7fb7815113822823d2d957f8e38eb745e8fbe6
Andrew Johnson <andrew.johnson@sportshubtech.com>
2018-11-08 15:52:21 -0600

    New build

---
+++ build/precache-manifest.238b6c8aee5ab6f80e192be94200b98f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf9d7fb035de8768d191",
+    "url": "/static/js/main.bf9d7fb0.chunk.js"
+  },
+  {
+    "revision": "6bf457de388ecfe5404f",
+    "url": "/static/js/1.6bf457de.chunk.js"
+  },
+  {
+    "revision": "bf9d7fb035de8768d191",
+    "url": "/static/css/main.615925b2.chunk.css"
+  },
+  {
+    "revision": "6bf457de388ecfe5404f",
+    "url": "/static/css/1.f72552cf.chunk.css"
+  },
+  {
+    "revision": "c4a985ba2b08227e86cdb3a221ddc09a",
+    "url": "/index.html"
+  }
+];
